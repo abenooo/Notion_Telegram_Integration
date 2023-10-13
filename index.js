67bde29e1594865a9ba5b1d292b39496ad3eb7a4
macboock <macboock@macboocks-MacBook-Pro.local>
2023-10-13 06:56:47 -0700

    Adding error message

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-const axios = require("axios")
-require('dotenv').config();
+const axios = require("axios");
+require("dotenv").config();
 
-const { NOTION_API_KEY, NOTION_DATABASE_ID, TELEGRAM_BOT_TOKEN } = process.env;
+const { NOTION_API_KEY, NOTION_DATABASE_ID } = process.env;
 
-async function fetchNotionData(){
+async function fetchNotionData() {
     const config = {
-        headers:{
+        headers: {
             'Authorization': `Bearer ${NOTION_API_KEY}`,
             'Notion-Version': '2022-06-28'
 
         }
     };
-try {
-    const response = await axios.post(`https://api.notion.com/v1/databases/${NOTION_DATABASE_ID}/query`, {}, config);
-return response.data.results;
-} catch (error) {
-    console.log('Error fetching Notion Data:', error);
-}
+    try {
+        const response = await axios.post(`https://api.notion.com/v1/databases/${NOTION_DATABASE_ID}/query`, {}, config);
+        return response.data.results;
+    } catch (error) {
+        console.log('Error fetching Notion Data:', error);
+    }
 }
 
+
+fetchNotionData()
+    .then(data => {
+        console.log("Fetched Data:", data);
+
+    })
+    .catch(error => {
+        console.log("Error in fetchNotion:", error)
+        console.log(`API Key: ${NOTION_API_KEY}`);
+        console.log(`Database ID: ${NOTION_DATABASE_ID}`);
+        console.log(`Telegram Bot Token: ${TELEGRAM_BOT_TOKEN}`);
+
+    });
