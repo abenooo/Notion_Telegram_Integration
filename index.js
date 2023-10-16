1f2fb6872a91960a9590bf1a78a5e570417bb145
macboock <macboock@macboocks-MacBook-Pro.local>
2023-10-16 07:21:01 -0700

    Displaying empty array

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const notion = new Client({
 });
 
 // Page IDs (replace with actual IDs)
-const EAGLE_TEAM_PAGE_ID = 'actual_id_here'; 
-const LION_TEAM_PAGE_ID = 'actual_id_here';
-const DESIGN_HUDDLE_PAGE_ID = 'actual_id_here';
-const ALL_HANDS_PAGE_ID = 'actual_id_here';
+const EAGLE_TEAM_PAGE_ID = 'afa9d18b4c724b29bd3532b3045c0f9e';
+const LION_TEAM_PAGE_ID = '33c665bf5e504ffaa4335b31b0eb3ba5';
+const DESIGN_HUDDLE_PAGE_ID = '136363ed304245c8be2d56ffdbb189f9';
+const ALL_HANDS_PAGE_ID = '6061f5f4a87540b4aad86a740c5fd2fc&pvs=12';
 
 // Async function to get data
 async function getData() {
@@ -32,7 +32,10 @@ async function getData() {
     const lionTeamPage = response.results.find(p => p.id === LION_TEAM_PAGE_ID);
     const designHuddlePage = response.results.find(p => p.id === DESIGN_HUDDLE_PAGE_ID);
     const allHandsPage = response.results.find(p => p.id === ALL_HANDS_PAGE_ID);
-
+    // Get page with tables
+    const page = response.results.find(p => p.id === 'a3422e5054c34ed695c2d272524bc888');
+ const data = getTableData(page);
+ console.log("new page" + data)
     // Get table data from each page
     const eagleTeamData = getTableData(eagleTeamPage);
     const lionTeamData = getTableData(lionTeamPage);
@@ -65,9 +68,37 @@ async function getData() {
 }
 
 // Helper to get table data from page
+// Helper to get table data from page
 function getTableData(page) {
-  // TODO: Fetch the table data based on the page object
-  return 'Your table data here';
+
+  const tableData = [];
+
+  if (!page || !page.children) {
+    return tableData;
+  }
+
+  page.children.forEach(table => {
+
+    const rows = table.table_rows;
+
+    rows.forEach(row => {
+      const rowData = {};
+
+      row.cells.forEach(cell => {
+        const column = cell.table_cell.title[0]?.plain_text;
+        const value = cell.table_cell.rich_text[0]?.plain_text;
+
+        rowData[column] = value;
+      });
+
+      tableData.push(rowData);
+
+    });
+
+  });
+
+  return tableData;
+
 }
 
 // Invoke getData()
